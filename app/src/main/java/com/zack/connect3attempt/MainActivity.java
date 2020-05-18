905fe9c38ea5cc469bfc1d198b429fff29cb8c68
Zack <warrior.killz@gmail.com>
2020-05-18 17:54:04 +0800

    First commit

---
+++ app/src/main/java/com/zack/connect3attempt/MainActivity.java
@@ -0,0 +1,23 @@
+package com.zack.connect3attempt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view) {
+        ImageView counter = (ImageView) view;
+        counter.setTranslationY(-1500);
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationYBy(1500).setDuration(300);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
